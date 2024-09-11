a9d07e5f2db8f2a79b55fa750490281d9019f71e
GuilhermeRuberto <rubertoguilherme@gmail.com>
2024-09-11 17:03:04 -0300

    modificando o imc utilizando uma funcao

--- imc.js
+++ imc.js
@@ -4,24 +4,28 @@ const teclado = require('prompt-sync')({sigint:true});
 peso = parseFloat(teclado("Digite o peso :"))
 altura = parseFloat(teclado("Digite a altura :"))
 
+function calc(peso, altura){
 altura = altura / 100;
-imc = peso/(altura*altura);
+return peso / (altura*altura);
 
-if(imc<18.5){
+}
+ let buffer = calc(peso, altura)
+
+if(buffer<18.5){
     console.log("Abaixo do Peso")
 }
-else if(imc>=18.5 & imc<=24.9){
+else if(buffer>=18.5 & buffer<=24.9){
     console.log("Peso Normal")
 }
-else if(imc>=25 & imc<=29){
+else if(buffer>=25 & buffer<=29){
     console.log("Acima do Peso")
 }
-else if(imc>=30 & imc<=35){
+else if(buffer>=30 & buffer<=35){
     console.log("Obesidade grau 1")
 }
-else if(imc>=35 & imc<=39.9){
+else if(buffer>=35 & buffer<=39.9){
     console.log("Obesidade grau 2")
 }
-else if(imc>=40){
+else if(buffer>=40){
     console.log("Obesidade grau 3")
 }
